e45b5f7f861240ef536e4a209d2fb126d4ca3972
Matthew Bajzek <mbajzek@mitre.org>
2017-11-27 18:02:17 -0500

    EOD commit: basic (beats, 5044) stable version

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# 1. initializes container with ENV variables set by OpenShift templates:
-#       - ${ELASTICSSEARCH_URL} -> elasticsearch.url
+# 1. initializes container with ENV variables set by OpenShift templates
 # 2. starts up logstash
 
 
 ##### 1 #####
 mv ${LOGSTASH_CONFIG}/logstash.yml ${LOGSTASH_CONFIG}/logstash.yml.old
 sed "s|__ELASTICSEARCH_URL__|$ELASTICSEARCH_URL|g" ${LOGSTASH_CONFIG}/logstash.yml.tmpl > ${LOGSTASH_CONFIG}/logstash.yml
+sed -i "s|__LOGSTASH_PIPELINE__|$LOGSTASH_PIPELINE|g" ${LOGSTASH_CONFIG}/logstash.yml
+rm ${LOGSTASH_PIPELINE}/logstash.conf
+sed "s|__ELASTICSEARCH_URL__|$ELASTICSEARCH_URL|g" ${LOGSTASH_PIPELINE}/logstash.conf.tmpl > ${LOGSTASH_PIPELINE}/logstash.conf
+rm ${LOGSTASH_PIPELINE}/logstash.conf.tmpl
 
 ##### 2 #####
 exec /usr/local/bin/docker-entrypoint
